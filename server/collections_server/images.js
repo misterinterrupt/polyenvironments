46c55e2c397117ccddd597607bac0ba180638ca1
Matt Howell <misterinterrupt@gmail.com>
2015-08-27 01:02:15 -0700

    fixed an idiot move, exposing bucket credentials -doh{0:#0{1}x}.format(1,6)

--- server/collections_server/images.js
+++ server/collections_server/images.js
@@ -10,8 +10,8 @@ Images = new FS.Collection("images", {
   stores: [
     new FS.Store.S3("thumbs", {
       //region: "my-s3-region", //optional in most cases
-      accessKeyId: "AKIAIKV5O2EXQOKFW4VA", //required if environment variables are not set
-      secretAccessKey: "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", //required if environment variables are not set
+      accessKeyId: process.env.accessKeyId, //required if environment variables are not set
+      secretAccessKey: process.env.secretAccessKey, //required if environment variables are not set
       bucket: "pe-net-images", //required
       ACL: "public-read", //optional, default is 'private', but you can allow public or secure access routed through your app URL
       //folder: "thumbs", //optional, which folder (key prefix) in the bucket to use 
@@ -22,8 +22,8 @@ Images = new FS.Collection("images", {
     }),
     new FS.Store.S3("images", {
       //region: "my-s3-region", //optional in most cases
-      accessKeyId: "AKIAIKV5O2EXQOKFW4VA", //required if environment variables are not set
-      secretAccessKey: "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", //required if environment variables are not set
+      accessKeyId: process.env.accessKeyId, //required if environment variables are not set
+      secretAccessKey: process.env.secretAccessKey, //required if environment variables are not set
       bucket: "pe-net-images", //required
       ACL: "public-read", //optional, default is 'private', but you can allow public or secure access routed through your app URL
       //folder: "full", //optional, which folder (key prefix) in the bucket to use 
